3053d3cf2777247638637b895a0578dca750a043
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-05-31 21:01:17 +0600

    added all events

--- src/pages/main/Events.jsx
+++ src/pages/main/Events.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Card from '../../components/card/Card';
 
 const Events = () => {
     return (
-        <div>
-            this is event page
+        <div className='py-12'>
+         <h1 className='text-center mb-6 text-xl md:text-3xl'>All Events </h1>
+            <Card></Card>
         </div>
     );
 };
